8004fa03056bc2b05a8f49d7a65c955d401bde79
Michał <nt.michal@gmail.com>
2020-05-04 16:42:58 +0200

    JSON
    Input: calendar1.json, calendar2.json,  output: output.json

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,3 +1,4 @@
+import json.JSON;
 import models.Calendar;
 import models.Meeting;
 import models.WorkingHours;
@@ -11,29 +12,28 @@ public class Main {
 
     public static void main(String[] args) throws IOException {
 
-        Calendar calendarEmployee1 = new Calendar();
-        calendarEmployee1.setWorkingHours(new WorkingHours(LocalTime.of(9,00),LocalTime.of(20,00)));
-        calendarEmployee1.addMeeting(new Meeting(LocalTime.of(9,00),LocalTime.of(10,30)));
-        calendarEmployee1.addMeeting(new Meeting(LocalTime.of(12,00),LocalTime.of(13,00)));
-        calendarEmployee1.addMeeting(new Meeting(LocalTime.of(16,00),LocalTime.of(18,30)));
+//        Calendar calendarEmployee1 = new Calendar();
+//        calendarEmployee1.setWorkingHours(new WorkingHours(LocalTime.of(9,00),LocalTime.of(20,00)));
+//        calendarEmployee1.addMeeting(new Meeting(LocalTime.of(9,00),LocalTime.of(10,30)));
+//        calendarEmployee1.addMeeting(new Meeting(LocalTime.of(12,00),LocalTime.of(13,00)));
+//        calendarEmployee1.addMeeting(new Meeting(LocalTime.of(16,00),LocalTime.of(18,30)));
 
-        Calendar calendarEmployee2 = new Calendar();
-        calendarEmployee2.setWorkingHours(new WorkingHours(LocalTime.of(10,00),LocalTime.of(18,30)));
-        calendarEmployee2.addMeeting(new Meeting(LocalTime.of(10,00),LocalTime.of(11,30)));
-        calendarEmployee2.addMeeting(new Meeting(LocalTime.of(12,30),LocalTime.of(14,30)));
-        calendarEmployee2.addMeeting(new Meeting(LocalTime.of(14,30),LocalTime.of(15,00)));
-        calendarEmployee2.addMeeting(new Meeting(LocalTime.of(16,00),LocalTime.of(17,00)));
-
-
-//        Calendar calendarEmployee1 = JSON.readCalendar("calendar1");
-//        Calendar calendarEmployee2 = JSON.readCalendar("calendar2");
+//        Calendar calendarEmployee2 = new Calendar();
+//        calendarEmployee2.setWorkingHours(new WorkingHours(LocalTime.of(10,00),LocalTime.of(18,30)));
+//        calendarEmployee2.addMeeting(new Meeting(LocalTime.of(10,00),LocalTime.of(11,30)));
+//        calendarEmployee2.addMeeting(new Meeting(LocalTime.of(12,30),LocalTime.of(14,30)));
+//        calendarEmployee2.addMeeting(new Meeting(LocalTime.of(14,30),LocalTime.of(15,00)));
+//        calendarEmployee2.addMeeting(new Meeting(LocalTime.of(16,00),LocalTime.of(17,00)));
 
 
+        Calendar calendarEmployee1 = JSON.readCalendar("calendar1");
+        Calendar calendarEmployee2 = JSON.readCalendar("calendar2");
+
+        //meetingDurationString HH:mm
         List<Meeting> output = MeetingAlgorithm.timeIntervals(calendarEmployee1, calendarEmployee2, "00:30");
         System.out.println(output.toString());
 
-//        JSON.saveAsJson(output,"output");
-
+        JSON.saveAsJson(output,"output");
 
     }
 
